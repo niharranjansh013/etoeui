fce28d6164a9ee4287b0016ebcc277c097d63c3f
Nihar Ranjan Sahoo <niharranjansh013@gmail.com>
2024-01-03 14:08:59 +0530

    header and footer changes

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Footer.module.css'
+export const Footer = () => {
+  return (
+    <div className='position-fixed bottom-0 bg-primary text-center text-white w-100'>
+      &copy; rights belongs to me
+    </div>
+  );
+}
+
+
---
+++ src/app/register/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './register.module.css'
+const Register = () => {
+  return (
+    <div className={style.clr}>
+      Register
+    </div>
+  );
+}
+
+export default Register;
